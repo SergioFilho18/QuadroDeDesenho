c7b2ea96b27c341d3fc46ee1fb981c2e4c2f9379
Sérgio Filho <barbosasergiofilho18@gmail.com>
2023-11-01 14:21:17 -0300

    :sparkles: feat: Added function to draw on the screen and added function to clear the screen.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Initial Data
 let currentColor = 'black';
 let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector("#tela");
 let ctx = screen.getContext('2d');
@@ -12,6 +14,7 @@ document.querySelectorAll('.colorArea .color').forEach(item =>{
 screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
 screen.addEventListener('mouseup', mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen);
 
 
 /* passo a passo para desenhar no canvas:
@@ -28,15 +31,37 @@ function colorClickEvent(e){
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.toggle('active');
 }
-function mouseDownEvent() {
+function mouseDownEvent(e) {
     canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
 }
 function mouseMoveEvent(e) {
     if(canDraw){
-        let pointX = e.pageX - screen.offsetLeft;
-        
+        draw(e.pageX, e.pageY);
     }
 }
 function mouseUpEvent(){
     canDraw = false;
+}
+function draw(x, y) {
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+
+function clearScreen() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 }

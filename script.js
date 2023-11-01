7b326ee722057640c89fb10681376dda4aba2850
Sérgio Filho <barbosasergiofilho18@gmail.com>
2023-11-01 09:53:05 -0300

    Added function of getting 'data-color' in target and remove/toggle class active

---
+++ script.js
@@ -0,0 +1,19 @@
+// Initial Data
+let currentColor = 'black';
+
+let screen = document.querySelector("#tela");
+let ctx = screen.getContext('2d');
+
+// Events
+document.querySelectorAll('.colorArea .color').forEach(item =>{
+    item.addEventListener('click', colorClickEvent);
+});
+
+// Functions
+function colorClickEvent(e){
+    let color = e.target.getAttribute('data-color');
+    currentColor = color;
+    
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.toggle('active');
+}

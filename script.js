93fece9edd40a1315083e7a103bd6888561d2cbe
Sérgio Filho <barbosasergiofilho18@gmail.com>
2023-11-01 10:57:01 -0300

    Added functions to start drawing on the canvas in the canvas tag

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Initial Data
 let currentColor = 'black';
+let canDraw = false;
 
 let screen = document.querySelector("#tela");
 let ctx = screen.getContext('2d');
@@ -8,6 +9,16 @@ let ctx = screen.getContext('2d');
 document.querySelectorAll('.colorArea .color').forEach(item =>{
     item.addEventListener('click', colorClickEvent);
 });
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+
+
+/* passo a passo para desenhar no canvas:
+- Quando o click do mouse ABAIXAR, ative o modo desenho.
+- Quando o mouse se MOVER, se o modo desenho estiver ativado, desenhe.
+- Quando o click do mouse LEVANTAR, desative o modo desenho.
+*/ 
 
 // Functions
 function colorClickEvent(e){
@@ -16,4 +27,16 @@ function colorClickEvent(e){
     
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.toggle('active');
+}
+function mouseDownEvent() {
+    canDraw = true;
+}
+function mouseMoveEvent(e) {
+    if(canDraw){
+        let pointX = e.pageX - screen.offsetLeft;
+        
+    }
+}
+function mouseUpEvent(){
+    canDraw = false;
 }
